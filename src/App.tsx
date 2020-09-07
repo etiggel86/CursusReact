ac17ea63ef35f0f230345a2d973e0cff4ca9033d
Eric-Jan Tiggelman <etiggelman@conclusion.nl>
2020-09-07 12:23:23 +0200

    aanpassing title

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Wijzig <code>src/App.tsx</code> en sla deze op om te herladen.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to Codaisseur
-        </a>
-      </header>
+      <main>
+        <Title />
+      </main>
     </div>
   );
 }
